e5147b24be812f48257a34ef45398b54a2786a4b
vincenzovolpe <vinci792010@gmail.com>
2019-11-27 17:34:38 +0100

    Caricamento prima versione esercizio funzionante con click anche sui pallini

---
+++ main.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+    // Mostra la prima immagine
+    $('.immagine-carosello:first').show();
+    // Dichiaro una variabile in cui memorizzo l'indice dell'iimagine attuale
+    var immagine_attuale = 0;
+    console.log(immagine_attuale);
+    // Aggungo il colore nero al pallino attuale corrispondente all'immagine immagine_attuale
+    $('.pallini>span:first').addClass('nero');
+
+    // Cambio immagine cliccando sul pallino
+    $('.punto').click(function() {
+        // Rimuovo il colore nero dal pallino mediante l'indice dell'immagine attuale
+        $('.punto').eq(immagine_attuale).removeClass('nero');
+        // Aggiungo il colore nero al pallino cliccato
+        $(this).addClass('nero');
+        // Nascondo l'immagine attualmente presente
+        $('.immagine-carosello').eq(immagine_attuale).hide();
+        // Aggiorno l'indice dell'immagine attuale
+        immagine_attuale = $(this).data('num') - 1;
+        console.log(immagine_attuale);
+        // Mostro l'immagine successiva (quella relativa al pallino cliccato)
+        $('.immagine-carosello').eq(immagine_attuale).show();
+    });
+
+    // Cambio immagine cliccando sulla freccia destra
+    $('.avanti').click(function() {
+        // Nascondo l'immagine attualmente presente
+        $('.immagine-carosello').eq(immagine_attuale).hide();
+        // Rimuovo il colore nero dal pallino mediante l'indice dell'immagine attuale
+        $('.punto').eq(immagine_attuale).removeClass('nero');
+        if (immagine_attuale < 5) {
+            immagine_attuale++;
+        } else {
+            immagine_attuale = 0;
+        }
+        // Mostro l'immagine successiva
+        $('.immagine-carosello').eq(immagine_attuale).show();
+        // Aggiungo il colore nero al pallino
+        $('.punto').eq(immagine_attuale).addClass('nero');
+    });
+
+    // Cambio immagine cliccando sulla freccia sinistra
+    $('.indietro').click(function() {
+        // Nascondo l'immagine attualmente presente
+        $('.immagine-carosello').eq(immagine_attuale).hide();
+        // Rimuovo il colore nero dal pallino mediante l'indice dell'immagine attuale
+        $('.punto').eq(immagine_attuale).removeClass('nero');
+        if (immagine_attuale > 0) {
+            immagine_attuale--;
+        } else {
+            immagine_attuale = 5;
+        }
+        // Mostro l'immagine successiva
+        $('.immagine-carosello').eq(immagine_attuale).show();
+        // Aggiungo il colore nero al pallino
+        $('.punto').eq(immagine_attuale).addClass('nero');
+    });
+});
